e0f683ffed46f0d520097326c0c6ca9cefaa1ae9
侯朋威 <houpengwei-dkh@bestpay.com.cn>
2024-05-20 14:30:02 +0800

    空框架-第一次提交

---
+++ small-red-book-start/src/main/java/hpw.small.red.book.start/SmallRedBookApplication.java
@@ -0,0 +1,12 @@
+package hpw.small.red.book.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallRedBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmallRedBookApplication.class, args);
+    }
+
+}
